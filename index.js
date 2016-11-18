e456f22c97e90f186b7b5e5498b036dce3345ecf
Cheok Swin Voon <cheoksv@my.ibm.com>
2016-11-18 14:36:58 +0800

    added help command

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.set('port', process.env.PORT || 3000);
 app.get('/reverse', function (req, res) {
    var input = req.query.text;
    res.end(input.split("").reverse().join(""));
-})
+});
+
+app.get('/help', function (req, res) {
+	var outputArray = [];
+	outputArray.push("reverse <ANY_TEXT> - Return the reverse of inputted text");
+	
+	res.contentType('application/json');
+	res.send(JSON.stringify(outputArray));
+});
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
